bb419d9d5c015c3394e8563ea9951d0bb7603063
Andres Vargas <andresandrangovargas@gmail.com>
2022-07-22 01:06:11 -0500

    second game

---
+++ Games/.dart_tool/flutter_build/f44d04ec25500ed122c409f3e160962b/kernel_snapshot.d
@@ -0,0 +1,1 @@
+D:\\SeptimoSemestre\\Textos\ Academicos\\Codigo\\Prueba\ juego\\hangman_flutter\\.dart_tool\\flutter_build\\f44d04ec25500ed122c409f3e160962b\\app.dill: D:\\SeptimoSemestre\\Textos\ Academicos\\Codigo\\Prueba\ juego\\hangman_flutter\\lib\\utils\\words.dart
---
+++ Games/lib/utils/game.dart
@@ -0,0 +1,6 @@
+class Game {
+  //adding the number of tries
+  static int tries = 0;
+  static List<String> selectedChar = [];
+  static int score = 6;
+}
